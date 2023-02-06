7ef46cf5ef4acc1dfe43eb4a5d75d5430fb93b3c
kkulik112 <kkulik112@gmail.com>
2023-02-06 21:22:43 +0100

    PostCreate, PostList components and cors handling

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import PostCreate from './PostCreate'
+import PostList from './PostList'
 
 const App = () => {
     return (
-        <div>
-            Blog app
+        <div className="container">
+            <h1>Create Post</h1>
+            <PostCreate />
+            <PostList />
         </div>
     )
 }
---
+++ client/src/PostCreate.js
@@ -0,0 +1,42 @@
+import React, {useState} from 'react'
+import axios from 'axios'
+
+
+const PostCreate = () => {
+
+    const mainAxios = axios.create({
+        baseURL: 'localhost:4000/'
+    })
+
+    const [title, setTitle] = useState('')
+
+    const onSubmit = async (e) => {
+        e.preventDefault()
+        try {
+
+            const payload = {title}
+
+            await mainAxios.post('http://localhost:4000/posts', payload)
+
+            setTitle('')
+        } catch (error) {
+            console.error('error')
+        }
+
+    }
+
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <div className="form-group">
+                    <label>Title</label>
+                    <input className="form-control" type="text" value={title} onChange={e => setTitle(e.target.value)}/>
+                </div>
+                <button className="btn btn-primary" type="submit">Submit</button>
+            </form>
+            <hr/>
+        </div>
+    )
+}
+
+export default PostCreate
