ab768c33d0a311bc26ed26f270a5b50663f7213e
kkulik112 <kkulik112@gmail.com>
2023-02-08 20:55:23 +0100

    Initial event-bus

---
+++ event-bus/index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const axios = require('axios')
+
+const app = express()
+
+app.use(bodyParser.json())
+
+app.post('/events', (req, res) => {
+    try {
+        const event = req.body
+
+        axios.post('http://localhost:4000/events', event)
+        axios.post('http://localhost:4001/events', event)
+        axios.post('http://localhost:4002/events', event)
+
+        res.send({status: 'OK'})
+    } catch (error) {
+        console.error(error)
+    }
+})
+
+app.listen(4005, () => {
+    console.log('Server running on port 4002')
+})
