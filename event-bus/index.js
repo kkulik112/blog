83891afa0b102c51004a980328bda8ca0776e080
kkulik112 <kkulik112@gmail.com>
2023-02-13 21:05:35 +0100

    Implemented event sync

--- event-bus/index.js
+++ event-bus/index.js
@@ -6,16 +6,18 @@ const axios = require('axios')
 const app = express()
 
 app.use(bodyParser.json())
-//app.use(cors())
+
+const events = []
 
 app.post('/events', (req, res) => {
     try{
 
         const event = req.body
+        events.push(event)
 
         axios.post('http://localhost:4001/events', event)
         axios.post('http://localhost:4000/events', event)
-        axios.post('http://localhost:4002/events', event)
+        axios.post('http://localhost:4002/events', event).catch(err => {})
         axios.post('http://localhost:4003/events', event)
         
         res.send({status: 'OK'})
@@ -23,6 +25,10 @@ app.post('/events', (req, res) => {
         console.log(error)
     }
 
+})
+
+app.get('/events', (req, res) => {
+    res.send(events)
 })
 
 app.listen(4005, () => {
--- query/index.js
+++ query/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const axios = require('axios')
 
 const app = express()
 app.use(cors())
@@ -8,24 +9,17 @@ app.use(bodyParser.json())
 
 const posts = {}
 
-app.get('/posts', (req, res) => {
-    res.send(posts)
-})
-
-app.post('/events', (req, res) => {
-    const {type, data} = req.body    
+const handleEvent = (type, data) => {
     if(type === 'PostCreated'){
         const {id, title,} = data
         posts[id] = {id, title, comments: []}
         console.log(`Event received: PostCreated`)
-        res.send(posts[id])
     }
 
     if(type === 'CommentCreated'){
         const {id, postId, content, status} = data
         posts[postId].comments.push({id, content, status})        
         console.log(`Event received: CommentCreated`)
-        res.send(posts[postId]['comments'])
 
     }
 
@@ -36,14 +30,34 @@ app.post('/events', (req, res) => {
         comment.status = status
         comment.content = content
         console.log(`Event received: CommentUpdated`)
-        res.send(posts[postId]['comments'])
     }
-                            
+}
+
+app.get('/posts', (req, res) => {
+    res.send(posts)
+})
+
+app.post('/events', (req, res) => {
+    const {type, data} = req.body    
+    
+    handleEvent(type, data)    
+    res.send({})                     
     
 })
 
-app.listen(4002, () => {
+
+app.listen(4002, async () => {
     console.log("Server running on port 4002")
+    try {
+        const res = await axios.get('http://localhost:4005/events')
+
+        for(const event of res.data){
+            console.log(`Processing event ${event.type}`)
+            handleEvent(event.type, event.data)
+        }
+    } catch (error) {
+        console.log(error.message)
+    }
 })
 
 module.exports = app
